f873d3a4ee396c4cfdadcb194556792073e6b1df
marnec <marnec@ryzenbeast.localdomain>
2024-08-22 10:53:26 +0200

    draft orbiiting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { Canvas } from '@react-three/fiber';
-import './App.css';
-import Sphere from './Sphere';
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
+import Sphere from "./Sphere";
+import { useRef } from "react";
 
 function App() {
+  let orbitingSphereRef = useRef()
+
+  useFrame(({ clock }) => {
+    let time = clock.getElapsedTime();
+    let radius = 2
+
+    
+
+  });
   return (
     <Canvas>
-    <ambientLight intensity={Math.PI / 2} />
-    <spotLight position={[10, 10, 10]} angle={Math.PI/2} decay={0} intensity={Math.PI} />
-    <Sphere position={[10, 10, 10]}></Sphere>
-    <Sphere position={[-1.2, 0, 0]} />
-  </Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={Math.PI / 2}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <Sphere position={[0, 0, 0]}></Sphere>
+      <Sphere position={[-2, 0, 0]} radius={0.5} orbitingRef={orbitingSphereRef}></Sphere>
+    </Canvas>
   );
 }
 
--- src/Sphere.tsx
+++ src/Sphere.tsx
@@ -1,25 +1,32 @@
-import { ThreeElements, useFrame } from '@react-three/fiber'
-import React, { useRef, useState } from 'react'
-import * as THREE from 'three'
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import * as THREE from "three";
 
-export default function Sphere(props: ThreeElements['mesh']) {
-  const ref = useRef<THREE.Mesh>(null!)
+type SphereProps = {
+  position: ThreeElements["mesh"]["position"];
+  radius?: number;
+  orbitingRef?: any
+};
 
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+export default function Sphere({ position, radius=1, orbitingRef }: SphereProps) {
+  const ref = useRef<THREE.Mesh>(null!);
 
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+
   return (
     <mesh
-      {...props}
+      position={position}
       ref={ref}
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      onPointerOut={(event) => hover(false)}
+    >
+      <sphereGeometry args={[radius, 32, 32]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
-  )
+  );
 }
