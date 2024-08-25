2228a1707f3802fc7c436ea19830c47fd6f1b516
marnec <marnec@ryzenbeast.localdomain>
2024-08-25 19:46:02 +0200

    try moving the skybox images directly in the public folder

--- packages/example/src/Skybox.tsx
+++ packages/example/src/Skybox.tsx
@@ -6,12 +6,12 @@ export default function Skybox() {
   const loader = new CubeTextureLoader();
 
   const texture = loader.load([
-    "/skybox/1.png", //left/right
-    "/skybox/3.png", // down/up
-    "/skybox/5.png", // down/up
-    "/skybox/6.png", //left/right
-    "/skybox/2.png", //front/back
-    "/skybox/4.png", //front/back
+    "skybox1.png", //left/right
+    "skybox3.png", // down/up
+    "skybox5.png", // down/up
+    "skybox6.png", //left/right
+    "skybox2.png", //front/back
+    "skybox4.png", //front/back
   ]);
 
   scene.background = texture;
